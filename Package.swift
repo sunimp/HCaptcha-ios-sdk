ecbedc5991cecd21fceca075468fd9578a5e3463
杨光 <yangguang@webull.com>
2024-08-22 11:09:31 +0800

    fix: typo

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "HCaptcha",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,7 +20,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.2.0"))
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.7.1"))
     ],
     targets: [
         .target(
